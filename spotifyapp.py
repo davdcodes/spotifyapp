99a56ca53f64ea0d3c5e9a4e645204df45918c04
David Navarro <davidgnav@gmail.com>
2025-05-17 16:01:53 +0000

    testing features

--- spotifyapp.py
+++ spotifyapp.py
@@ -9,13 +9,26 @@ CLIENTSECRET = "305aed74adb64331bb29d595b7915d6c"
 REDIRECT = "https://crunchlist.streamlit.app/"
 SCOPE = "user-library-read user-read-playback-state user-modify-playback-state"
 
+cache = spotipy.cache_handler.MemoryCacheHandler()
 
 sp_oauth = SpotifyOAuth(client_id = CLIENTID,
                         client_secret = CLIENTSECRET,
                         redirect_uri = REDIRECT,
-                        scope = SCOPE)
+                        scope = SCOPE,
+                        cache_handler = cache,
+                        show_dialog = True
+)
+if "token_info" not in st.session_state:
+    token_indo = auth manager.get_cached_token()
+    if not token_info:
+        auth_url = auth_manager.get_authorize_url()
+        st.write("Please log in to Spotify: ")
+        st.markdown(f"[Login to Spotify]({auth_url})")
+    else:
+        st.session_state.token_info = token_info
 
-st.title("Spotify Login")
 
-if st.button("Login with spotify"):
-    auth_url = sp_oauth.get_authorize_url()
+if "token_info" in st.session_state:
+    sp = spotipy.Spotify(auth_manager = auth_manager)
+    user = sp.current_user()
+    st.success(f"Loggin in as {user['display_name']}")
